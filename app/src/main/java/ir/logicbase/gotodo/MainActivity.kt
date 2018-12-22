426f29a6bacd55e0821f0d0fbf946e466e299a61
Mahdi <m.javaherisaber@gmail.com>
2018-12-22 10:25:28 +0330

    Migrate to Androidx

--- app/src/main/java/ir/logicbase/gotodo/MainActivity.kt
+++ app/src/main/java/ir/logicbase/gotodo/MainActivity.kt
@@ -1,11 +1,10 @@
 package ir.logicbase.gotodo
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/ir/logicbase/gotodo/MainActivityFragment.kt
+++ app/src/main/java/ir/logicbase/gotodo/MainActivityFragment.kt
@@ -1,6 +1,5 @@
 package ir.logicbase.gotodo
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +8,7 @@ import android.view.ViewGroup
 /**
  * A placeholder fragment containing a simple view.
  */
-class MainActivityFragment : Fragment() {
+class MainActivityFragment : androidx.fragment.app.Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
