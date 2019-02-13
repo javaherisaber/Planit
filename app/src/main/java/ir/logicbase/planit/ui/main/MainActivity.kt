63de38e2eb8330d37b89d5db9bb7d1a0c8c62806
Mahdi <m.javaherisaber@gmail.com>
2019-02-13 11:13:56 +0330

    Init color, style and splash screen

--- app/src/main/java/ir/logicbase/planit/ui/main/MainActivity.kt
+++ app/src/main/java/ir/logicbase/planit/ui/main/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         Pushe.initialize(this, true)
